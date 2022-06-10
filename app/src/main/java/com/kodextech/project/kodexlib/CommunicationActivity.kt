ad87b4a2cef6bfa37a217105f68cd44ea04fd3a0
kiranyaduvanshi <kirandeveloper786@gmail.com>
2022-06-10 14:44:14 +0530

    Looking Good

---
+++ app/src/main/java/com/kodextech/project/kodexlib/CommunicationActivity.kt
@@ -0,0 +1,11 @@
+package com.kodextech.project.kodexlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CommunicationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_communication)
+    }
+}
