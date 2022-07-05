3c729e1528e20d5ac3bf513cf7074617161802ac
kiranyaduvanshi <kirandeveloper786@gmail.com>
2022-07-05 18:32:03 +0530

    dfdfdF

--- app/src/main/java/com/kodextech/project/kodexlib/ui/main/quatation/QuotationScreenActivity.kt
+++ app/src/main/java/com/kodextech/project/kodexlib/ui/main/quatation/QuotationScreenActivity.kt
@@ -1,18 +1,16 @@
 package com.kodextech.project.kodexlib.ui.main.quatation
 
+import android.content.Intent
 import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import com.kodextech.project.kodexlib.R
 import com.kodextech.project.kodexlib.base.BaseActivity
 import com.kodextech.project.kodexlib.databinding.ActivityQuotationScreenBinding
+import com.kodextech.project.kodexlib.ui.main.customer.CustomerListing
 
 class QuotationScreenActivity : BaseActivity() {
 
     private var binding: ActivityQuotationScreenBinding? = null
-
-
-
-
     override fun onSetupViewGroup() {
         mViewGroup = binding?.qutationCustomer
 
@@ -20,6 +18,11 @@ class QuotationScreenActivity : BaseActivity() {
 
     override fun setupContentViewWithBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_quotation_screen)
+
+        binding?.tvCustomerName?.setOnClickListener {
+            val intent = Intent(this, SearchCustomerActivity::class.java)
+            startActivity(intent)
+        }
 
 
 
